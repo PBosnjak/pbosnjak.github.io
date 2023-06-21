9f30e18f2284bd914f936a6b9bc0e48dab058120
Petar Bošnjak <petar.bosnjak@yahoo.com>
2023-06-21 17:14:23 +0200

    Add timer

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@ $( "#open" ).on( "click", function() {
     $(".bottom").toggleClass( "bottom-transform-partial" );
     $(".inner-top").toggleClass( "inner-top-transform-partial" );
     $(".inner-bottom").toggleClass( "inner-bottom-transform-partial" );
+    $(".text").toggleClass( "text-transform-partial" );
 
     // $(".open").toggleClass( "open-transform-full" );
     // $(".bottom").toggleClass( "bottom-transform-full" );
     // $(".inner-top").toggleClass( "inner-top-transform-full" );
     // $(".inner-bottom").toggleClass( "inner-bottom-transform-full" );
-    $(".text").toggleClass( "text-transform-full" );
+    // $(".text").toggleClass( "text-transform-full" );
+    // $(".timer").toggleClass( "timer-transform-full" );
 });
