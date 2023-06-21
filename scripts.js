c733b326721052b5a00895970286f6e675b7b959
Petar Bošnjak <petar.bosnjak@yahoo.com>
2023-06-21 16:35:26 +0200

    Add partial transform

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 $( "#open" ).on( "click", function() {
-    $(".open").toggleClass( "open-transform-full" );
-    $(".bottom").toggleClass( "bottom-transform-full" );
-    $(".inner-top").toggleClass( "inner-top-transform-full" );
-    $(".inner-bottom").toggleClass( "inner-bottom-transform-full" );
+    $(".open").toggleClass( "open-transform-partial" );
+    $(".bottom").toggleClass( "bottom-transform-partial" );
+    $(".inner-top").toggleClass( "inner-top-transform-partial" );
+    $(".inner-bottom").toggleClass( "inner-bottom-transform-partial" );
+
+    // $(".open").toggleClass( "open-transform-full" );
+    // $(".bottom").toggleClass( "bottom-transform-full" );
+    // $(".inner-top").toggleClass( "inner-top-transform-full" );
+    // $(".inner-bottom").toggleClass( "inner-bottom-transform-full" );
     $(".text").toggleClass( "text-transform-full" );
 });
